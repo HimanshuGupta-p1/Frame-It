a507cbff491c3959404d24d67f4ed00db78cebff
Himanshu Gupta <87177106+HimanshuGupta-p1@users.noreply.github.com>
2023-10-18 23:53:41 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -24,10 +24,11 @@ todos = [{'title': 'class_start', 'desc': '9.05'},
 def greet():
     return "PSIT 4A 4B, iys FastAPI time "
 
+# List of all the todos
 @app.get('/api/v1/todos')
 def getAllTodos():
     return todos
-
+# Get a specific todo
 @app.get('/api/v1/todos/{title}')
 def getTodo(title: str):
     result_todo = {}
@@ -36,6 +37,7 @@ def getTodo(title: str):
             result_todo = todo
     return result_todo
 
+# Add a new todo to the list
 @app.post('/api/v1/todos')
 def createTodo(todo: dict):
     todos.append(todo)
@@ -44,6 +46,7 @@ def createTodo(todo: dict):
         'message': 'todo has been added'
     }
 
+# Delete a specific todo
 @app.delete('/api/v1/todos/delete/{title}')
 def deleteTodo(title: str):
     result_todo = {}
@@ -60,7 +63,7 @@ def deleteTodo(title: str):
         'message': 'todo could not be found'
     }
 
-
+# Update any todo 
 @app.put('/api/v1/todos/{title}/{desc}')
 def updateTodo(title: str, desc: str):
     for todo in todos:
